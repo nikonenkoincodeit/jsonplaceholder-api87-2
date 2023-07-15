6a5469ed5c5948fbe1ebaf41b5c1801f7ab323fa
Jgodka <jdzinska77@gmail.com>
2023-07-15 11:44:39 +0300

    add onClick

--- src/components/home.js
+++ src/components/home.js
@@ -9,8 +9,13 @@ function init() {
   getData("users")
     .then((resp) => {
       const markup = markupUsers(resp);
-      console.log(markup);
+
       addMarkUp(tBodyEl, markup);
     })
     .catch((err) => console.log(err.message));
+}
+tBodyEl.addEventListener("click", onClick);
+function onClick(evt) {
+  console.log(evt.target.closest("tr").dataset.userid);
+  location.href = `user.html?userid=${evt.target.closest("tr").dataset.userid}`;
 }
