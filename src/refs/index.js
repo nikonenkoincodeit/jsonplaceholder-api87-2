312239ce2f2fcb33246aa1ae58532f8cddcd7b18
Tosha177777 <slavkoanton4@gmail.com>
2023-08-13 16:43:33 +0300

    Update index.js

--- src/refs/index.js
+++ src/refs/index.js
@@ -1,3 +1,4 @@
 export const usersEl = document.querySelector(".js-tbody");
 export const tableUserEl = document.querySelector(".js-user-table");
 export const albumEl = document.querySelector(".js-album");
+export const photosEl = document.querySelector(".js-list-song");
