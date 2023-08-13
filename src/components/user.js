ed9a0727ccb7f6554b7b95b9893164cb236136a3
MaksymMoskalov <max.godnavi133@gmail.com>
2023-08-13 16:25:39 +0300

    To Album added

--- src/components/user.js
+++ src/components/user.js
@@ -1,14 +1,31 @@
 import { getData } from "../api";
-import { markupByUserId } from "../markup";
+import { markupByUserId, createListAlbum } from "../markup";
 import { addMarkup } from "../helpers";
-import { tableUserEl } from "../refs";
+import { tableUserEl, albumEl } from "../refs";
 
 const search = location.search;
 const paramsString = new URLSearchParams(search);
 const userId = paramsString.get("userId");
 console.log(userId);
 
-getData("users/" + userId).then((res) => {
-  const markup = markupByUserId(res);
-  addMarkup(markup, tableUserEl);
-});
+getData("users/" + userId)
+  .then((res) => {
+    const markup = markupByUserId(res);
+    addMarkup(markup, tableUserEl);
+  })
+  .catch((err) => console.error(err.message));
+
+getData("albums?userId=" + userId)
+  .then((resp) => {
+    const albumMarkup = createListAlbum(resp);
+    addMarkup(albumMarkup, albumEl);
+  })
+  .catch((err) => console.error(err.message));
+
+albumEl.addEventListener("click", onAlbumClick);
+
+function onAlbumClick(event) {
+  const album = event.target.closest(".js-list-user-album");
+  const albumId = album.dataset.id;
+  location.href = "album.html?albumId=" + albumId;
+}
--- src/refs/index.js
+++ src/refs/index.js
@@ -1,2 +1,3 @@
 export const usersEl = document.querySelector(".js-tbody");
 export const tableUserEl = document.querySelector(".js-user-table");
+export const albumEl = document.querySelector(".js-album");
